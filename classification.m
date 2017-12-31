c80f01923fc20eed6ac6190e743242b18dcf7019
Mariusz Wachowicz <wachowiczmariusz@gmail.com>
2017-12-31 01:21:30 +0100

    Import data

---
+++ classification.m
@@ -0,0 +1,13 @@
+%% preprocessing
+all_data = import_data('transfusion.data');
+samples = all_data(:,1:4);
+reference = all_data(:,5);
+
+% hold on;
+% grid on;
+% plot(samples{:,1}, reference{:,1}, 'rx');
+
+%% processing
+
+
+%% postprocessing
